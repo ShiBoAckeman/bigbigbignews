dad2616decfba7cad338b9a8a4ec0aee41d06b9d
shibo <shibo@cast.cn>
2020-02-18 21:24:01 +0800

    仕博的第六次提交

--- static/admin/js/article-modify.js
+++ static/admin/js/article-modify.js
@@ -11,7 +11,19 @@ $('#listBox').on('click', '.modify', function () {
         success: function (response) {
             // alert('11')
             response = response.data;
-            console.log(response)
+            // console.log(response)
+            $.ajax({
+                url: 'http://localhost:8080/api/v1/admin/category/list',
+                success: function (categories) {
+                    // console.log(categories);
+                    response.categories = categories.data
+                    console.log(response);
+
+                    var html = template('modifyTpl', response)
+                    console.log(html);
+
+                }
+            })
             // location.href = "http://localhost:8080/admin/article_release.html";
             // var html = template('modifyTpl', response);
             // console.log(html)
